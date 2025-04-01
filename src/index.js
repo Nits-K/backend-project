0198a32caf6f9d9c2a7ec57a8fb62c6b3c362942
Nitin Kant <kantnitin7@gmail.com>
2025-04-01 18:13:58 +0530

    util files added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
-import  dotenv  from "dotenv";
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
-})
-connectDB();
+  path: "./env",
+});
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on PORT : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection failed :", error);
+  });
 
 // import express from "express";
 
@@ -27,6 +39,6 @@ connectDB();
 // {
 //     console.log("some error has occured");
 //     throw err;
-    
+
 // }
 // })()
