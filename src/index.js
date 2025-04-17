1823095fe493fa6eb82f1d2bf0ce86170e3dfda5
Nitin Kant <kantnitin7@gmail.com>
2025-04-17 13:18:39 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 // import { DB_NAME } from "./constants";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
@@ -12,6 +13,7 @@ connectDB()
       console.log("ERR:", error);
       throw error;
     });
+
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running on PORT : ${process.env.PORT}`);
     });
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -10,10 +10,12 @@
 // };
 
 // Same above task using promises
-const asyncHandler =(requestHandler)=>{
- (req,res,next)=>{
+const asyncHandler = (requestHandler)=>{
+ return (req,res,next)=>{
     Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
  }
 }
+
+
 
 export { asyncHandler };
