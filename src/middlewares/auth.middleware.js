e4bdff24bb908cb89067ae737a5891692b4d4fe8
Nitin Kant <kantnitin7@gmail.com>
2025-04-18 14:32:03 +0530

    add refreshAccessToken endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,7 +7,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
+      req.header("Authorization")?.replace("Bearer ","");
 
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
@@ -26,5 +26,6 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     next();
   } catch (error) {
     throw new ApiError(401, error?.message || "Invalid access token");
+    
   }
 });
